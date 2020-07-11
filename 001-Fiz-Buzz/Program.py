aab952566e4455cc789ece22d770835cfd08cdbd
Alpha-1729 <anilakskumar1729@gmail.com>
2020-07-11 20:24:22 +0530

    completed Fizz-Buzz

---
+++ 001-Fiz-Buzz/Program.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# Fiz-Buzz
+
+"""
+>>>>STEPS:
+        Made a dictionary.
+        First add multiples of the number in list "C" as key and value as corresponding word in the list "W".
+        If same multiple appear, concatenate it with the existing value in the dictionary.
+        Made a final list and iterate i from 1-101 and fill it with:
+            value in dic, if key is in dic.
+            str(i), else
+        And done join method in python.
+        
+"""
+N=int(input())
+C=list(map(int,(input().split())))
+W=list(input().split())
+dic={}
+for i in range(N):
+    for j in range(C[i],101,C[i]):
+        if j not in dic:
+            dic[j]=W[i]
+        else:
+            dic[j]=dic[j] + " " + W[i]
+print(', '.join([dic[i] if i in dic else str(i) for i in range(1,101)]))
--- git_push.bat
+++ git_push.bat
@@ -2,5 +2,5 @@ python check_empty_folder.py
 set /p message="Message to commit: "
 git add *
 git commit  -m "%message%"
-git push
+git push origin master
 pause
