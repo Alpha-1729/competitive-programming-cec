07b36adbdd8283abbf02afbd6b04521611456b69
Alpha-1729 <anilakskumar1729@gmail.com>
2020-07-12 19:34:03 +0530

    added longest substring withoout repeating characters

---
+++ 002-Longest-Substring-Without-Repeating-Characters/Program.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# Longest-Substring-Without-Repeating-Characters
+
+"""
+>>>>STEPS:
+        Made a set.
+        loop through all character in the string.
+        if character not in string:
+            add it to the set
+            update length
+        else:
+            removing from set
+        return the length.
+>>>>
+>>>>
+>>>>
+>>>>
+"""
+def maxLength(data):
+    store=set()
+    length=0
+    for j in range(len(data)):
+        if data[j] in store:
+            store.remove(data[j])
+        else:
+            store.add(data[j])
+            length=max(len(store),length)
+    return length
+    
+data=input()
+print(maxLength(data))   
