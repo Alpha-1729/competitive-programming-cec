7f5849270be80ab98f634730876069f8fd463f6e
Alpha-1729 <anilakskumar1729@gmail.com>
2020-07-16 14:27:40 +0530

    completed Comparing Powers

---
+++ 006-Comparing-Powers/Program.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Comparing-Powers
+
+"""
+>>>>STEPS:
+        taking the log of the both and compare.
+>>>>
+>>>>
+>>>>
+>>>>
+"""
+import math
+def takeLog(b,p):
+    return b * math.log(p)
+b1,p1,b2,p2=map(int,input().split())
+if ( takeLog(b1,p1) > takeLog(b2,p2)):print("{}^{} is greater".format(b1,p1))
+elif (takeLog(b1,p1) < takeLog(b2,p2)):print("{}^{} is greater".format(b2,p2))
+else:print("Both are equal."); 
